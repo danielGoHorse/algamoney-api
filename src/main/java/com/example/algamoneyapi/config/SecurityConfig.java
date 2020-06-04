16140ab8d8780290e2bd5d19c0885000f8783457
DANIEL HENRIQUE GOTARDO <daniel.gotardo@kaspper.com>
2020-06-04 16:48:11 -0300

    SegurancaOAuth2

--- src/main/java/com/example/algamoneyapi/config/SecurityConfig.java
+++ src/main/java/com/example/algamoneyapi/config/SecurityConfig.java
@@ -1,33 +1,37 @@
 package com.example.algamoneyapi.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManager();
+	}
+	
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password("123").roles("ADMIN");
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.anyRequest().authenticated()
-		.and()
-		.httpBasic()
-		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
