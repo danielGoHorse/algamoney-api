42aa321804dbf43b2ec18d710e8480905eb4c1a3
DANIEL HENRIQUE GOTARDO <daniel.gotardo@kaspper.com>
2020-06-21 01:45:24 -0300

    procfile3

--- src/main/java/com/example/algamoneyapi/config/ResourceServerConfig.java
+++ src/main/java/com/example/algamoneyapi/config/ResourceServerConfig.java
@@ -43,7 +43,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/categorias").permitAll()
-				.antMatchers("/oauth/token").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
